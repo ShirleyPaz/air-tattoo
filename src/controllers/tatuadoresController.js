073708c026901a8fc5b20d1bb06e2c7fc471bc66
shirleypaz <shirley.pazz@gmail.com>
2019-12-14 12:34:29 -0300

    added id to tatuador schema

--- src/controllers/tatuadoresController.js
+++ src/controllers/tatuadoresController.js
@@ -7,18 +7,32 @@ exports.get = (req, res) => {
             res.status(200).send(tatuadores);
         })
         .catch((e) => {
-            res.status(200).send(e)
+            res.status(500).send(e)
         })
   };
 
   exports.post = (req, res) => {
-    let tatuador = new Tatuador(req.body);
-
-    tatuador.save()
+    Tatuador.find()
+    .then((tatuadores) => {
+        res.status(200).send(tatuadores);
+        const nextId = tatuadores.length == 0 ? 0 : tatuadores[tatuadores.length-1].id + 1
+        let tatuador = new Tatuador(req.body);
+        tatuador.id = nextId;
+        tatuador.save()
         .then(() => {
             return res.status(201).send({tatuador})
         })
         .catch((err) => {
             return res.status(500).send({ message: err });
         })
+    })
+    .catch((e) => {
+        res.status(500).send(e)
+    })
   }
+
+    
+
+    
+
+    
--- src/models/tatuadoresSchema.js
+++ src/models/tatuadoresSchema.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const tatuadorSchema = new Schema(
-    {
+    {   
+        id: { type: Number, required: true },
         nome: { type: String, required: true },
         email: { type: String, required: true },
         estilo: { type: String, required: true },
