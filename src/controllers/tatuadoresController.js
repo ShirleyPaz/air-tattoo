b3af6093761e813f984a0ba7110e4a5e53c0a8f1
shirleypaz <shirley.pazz@gmail.com>
2019-12-15 08:53:00 -0300

    removed double catch

--- src/controllers/tatuadoresController.js
+++ src/controllers/tatuadoresController.js
@@ -1,38 +1,30 @@
 const Tatuador = require('../models/tatuadoresSchema')
 
-
 exports.get = (req, res) => {
-        Tatuador.find()
+    Tatuador.find()
         .then((tatuadores) => {
             res.status(200).send(tatuadores);
         })
         .catch((e) => {
             res.status(500).send(e)
         })
-  };
+};
 
-  exports.post = (req, res) => {
+exports.post = (req, res) => {
     Tatuador.find()
-    .then((tatuadores) => {
-        res.status(200).send(tatuadores);
-        const nextId = tatuadores.length == 0 ? 0 : tatuadores[tatuadores.length-1].id + 1
-        let tatuador = new Tatuador(req.body);
-        tatuador.id = nextId;
-        tatuador.save()
-        .then(() => {
-            return res.status(201).send({tatuador})
+        .then((tatuadores) => {
+            const nextId = tatuadores.length == 0 ? 0 : tatuadores[tatuadores.length - 1].id + 1
+            let tatuador = new Tatuador(req.body);
+            tatuador.id = nextId;
+            tatuador.save()
+            return res.status(201).send({ tatuador })
         })
-        .catch((err) => {
-            return res.status(500).send({ message: err });
+        .catch((e) => {
+            res.status(500).send(e)
         })
-    })
-    .catch((e) => {
-        res.status(500).send(e)
-    })
-  }
+}
 
-    
 
-    
 
-    
+
+
