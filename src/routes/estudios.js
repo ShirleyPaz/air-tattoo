808f24ea4e4d4b01fa47949f098e8c2383076b1d
shirleypaz <shirley.pazz@gmail.com>
2019-12-15 21:32:36 -0300

    updated estudios routes

--- src/routes/estudios.js
+++ src/routes/estudios.js
@@ -5,11 +5,14 @@ const controller = require("../controllers/estudiosController");
 // define rotas para clientes
 router.get("/", controller.get);
 router.get("/:name/agenda/:data", controller.getAgendaByDate);
+router.get("/:name/agenda/:data/eventos", controller.getEventosByDate);
+router.get("/:name/agenda/:data/eventos/:eventoId", controller.getEventoByDateAndId);
 router.get("/:name/agenda", controller.getAgendaByEstudioName);
 router.get("/:name", controller.getByName);
 router.post("/", controller.post);
 router.post("/:name/agenda/:data", controller.postEventoByDate);
 router.post("/:name/agenda", controller.postAgendaByEstudioName);
-
+router.put("/:name/agenda/:data/:eventoId", controller.updateEventById);
+router.delete("/:name/agenda/:data/:eventoId", controller.deleteEventById);
 
 module.exports = router;
