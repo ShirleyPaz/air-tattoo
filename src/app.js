258c9d4e8154456f1a23f44b954b752ea399f148
shirleypaz <shirley.pazz@gmail.com>
2019-12-16 18:59:18 -0300

    added middlewares - cors

--- src/app.js
+++ src/app.js
@@ -4,14 +4,14 @@ const bodyParser = require('body-parser');
 const app = express();
 
 // Conexão com BD
-mongoose.connect('mongodb+srv://airtattoo:LdHpNNOdvuHmtmRn@airttattoo-1skm7.mongodb.net/',{
-useNewUrlParser: true
+mongoose.connect('mongodb+srv://airtattoo:LdHpNNOdvuHmtmRn@airttattoo-1skm7.mongodb.net/', {
+    useNewUrlParser: true
 });
 
 let db = mongoose.connection;
 
 db.on('error', console.log.bind(console, 'connection error: '));
-db.once('open', () => {console.log('conexão realizada')});
+db.once('open', () => { console.log('conexão realizada') });
 
 // Define Rotas
 const index = require('./routes/index');
@@ -19,13 +19,23 @@ const tatuadores = require('./routes/tatuadores');
 const estilos = require('./routes/estilos');
 const estudios = require('./routes/estudios');
 
+// Define middlewares
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    next();
+});
+
 // bodyParser
 app.use(express.static("public"));
 app.use(bodyParser.json());
 
 // Rotas
 app.use("/", index);
-app.use('/tatuadores',tatuadores);
+app.use('/tatuadores', tatuadores);
 app.use('/estilos', estilos);
 app.use('/estudios', estudios);
 
