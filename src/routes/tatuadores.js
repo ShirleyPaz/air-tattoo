2bfc1416d65d844d7d3fa15a57c969f18a83ec55
shirleypaz <shirley.pazz@gmail.com>
2019-12-16 04:31:00 -0300

    added put and delete routes to tatuadores

--- src/routes/tatuadores.js
+++ src/routes/tatuadores.js
@@ -5,5 +5,7 @@ const controller = require("../controllers/tatuadoresController");
 // define rotas para clientes
 router.get("/", controller.get);
 router.post("/", controller.post);
+router.put("/:id", controller.updateById);
+router.delete("/", controller.deleteByName);
 
 module.exports = router;
