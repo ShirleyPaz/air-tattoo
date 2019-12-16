22291d55fd88b0d35f1215a2806ab2ec59fd8b17
shirleypaz <shirley.pazz@gmail.com>
2019-12-16 04:32:35 -0300

    Added put and delete methods

--- src/controllers/tatuadoresController.js
+++ src/controllers/tatuadoresController.js
@@ -25,6 +25,46 @@ exports.post = (req, res) => {
         })
 }
 
+exports.deleteByName = async (req, res) => {
+    const nome = req.body.nome.toLowerCase();
+    await Tatuador.find({ nome })
+        .then((tatuador) => {
+            if (tatuador.length === 0) {
+                return res.sendStatus(404).send("NOT FOUND");;
+            }
+            tatuador[0].remove(err => {
+                if (!err) {
+                    res.status(200).send("SUCCESS");
+                }
+            });
+        })
+        .catch((e) => {
+            res.status(500).send('AN ERROR HAS OCCURRED. TRY AGAIN LATER.')
+        })
+};
+
+exports.updateById = async (req, res) => {
+
+    try {
+        const idParam = req.params.id;
+        let novoTatuador = req.body;
+        novoTatuador.nome = req.body.nome.toLowerCase();
+        await Tatuador.updateOne(
+            { id: Number(idParam) },
+            { $set: novoTatuador })
+            .then((cliente) => {
+                if (!cliente) {
+                    return res.sendStatus(404).send("NOT FOUND");;
+                }
+                res.status(200).send("SUCCESS");
+            })
+            .catch((e) => {
+                res.status(500).send('AN ERROR HAS OCCURRED. TRY AGAIN LATER.')
+            });
+    } catch (e) {
+        res.status(500).send('AN ERROR HAS OCCURRED. TRY AGAIN LATER.')
+    }
+};
 
 
 
