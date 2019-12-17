61cf550e8b5e01d7fc658b834892212d5222bc93
shirleypaz <shirley.pazz@gmail.com>
2019-12-16 23:04:48 -0300

    removed comments

--- src/models/estudiosSchema.js
+++ src/models/estudiosSchema.js
@@ -1,39 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-// const EstudiosSchema = new Schema(
-//     {
-//         id: { type: Number, required: true },
-//         nome: { type: String, required: true },
-//         email: { type: String, required: true },
-//         endLogradouro: { type: String, required: true },
-//         endNumero: { type: Number, required: true },
-//         cep: { type: String, required: true },
-//         agenda: [
-//             {
-//                 data: { type: Date, required: true },
-//                 horaInicio: { type: Number, required: true },
-//                 horaFinal: { type: Number, required: true },
-//                 eventos: [
-//                     {
-//                         id: { type: Number, required: true },
-//                         horaInicio: { type: Number, required: true },
-//                         horaFinal: { type: Number, required: true },
-//                         tatuador: { type: String },
-//                         tipo: { type: String, required: true },
-//                     }
-//                 ],
-//             }]
-//     }, {
-//     versionKey: false
-// });
-
-// const Estudios = mongoose.model('Estudios', EstudiosSchema);
-
-// module.exports = Estudios;
-
 const Evento = new Schema({
-    // id: Number, 
     horaInicio: { type: Number, required: true },
     horaFinal: { type: Number, required: true },
     tatuador: { type: String},
